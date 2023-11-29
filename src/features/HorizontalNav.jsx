c9aa0259caa8b4a168de69987429a76d8fc26343
mpollard33 <mpollard33@gmail.com>
2023-11-29 14:35:07 -0700

    updated horizontalNav and added styling

--- src/features/HorizontalNav.jsx
+++ src/features/HorizontalNav.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HorizontalNav = () => {
   return (
-    <nav>
+    <nav className="horizontal-nav">
       <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/products'>Products</Link></li>
-        <li><Link to='/account'>Account</Link></li>
-        <li><Link to='/cart'>Cart</Link></li>
+        <li><Link to='/' className="nav-link">Home</Link></li>
+        <li><Link to='/products' className="nav-link">Products</Link></li>
+        <li><Link to='/account' className="nav-link">Account</Link></li>
+        <li><Link to='/cart' className="nav-link">Cart</Link></li>
       </ul>
     </nav>
   );
--- src/features/VerticalNav.jsx
+++ src/features/VerticalNav.jsx
@@ -1,20 +1,17 @@
-// VerticalNav component
-import React from "react";
-import { Link } from "react-router-dom";
-import '../index.css'
+import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 const VerticalNav = () => {
   return (
-    <nav >
-      <ul className="vertical-nav">
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/products'>Products</Link></li>
-        <li><Link to='/account'>Account</Link></li>
-        <li><Link to='/cart'>Cart</Link></li>
+    <nav className="vertical-nav">
+      <ul>
+        <li><Link to='/' className="nav-link">Home</Link></li>
+        <li><Link to='/products' className="nav-link">Products</Link></li>
+        <li><Link to='/account' className="nav-link">Account</Link></li>
+        <li><Link to='/cart' className="nav-link">Cart</Link></li>
       </ul>
     </nav>
   );
 };
-
 
 export default VerticalNav;
