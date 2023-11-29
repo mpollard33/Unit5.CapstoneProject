4daac4f8ad33650e10d2745d81d244eeca051408
mpollard33 <mpollard33@gmail.com>
2023-11-28 22:52:48 -0700

    created productsApi

---
+++ src/features/products/productsApi.js
@@ -0,0 +1,14 @@
+import api from '../../store/api';
+
+const productsApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => '/products',
+    }),
+    getProductById: builder.query({
+      query: (id) => `/products/${id}`,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery, useGetProductByIdQuery } = productsApi;
