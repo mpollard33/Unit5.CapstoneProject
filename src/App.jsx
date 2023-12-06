2236dbd481a8d50415c546e8edee30e60d7b7246
mpollard33 <mpollard33@gmail.com>
2023-12-05 22:46:35 -0700

    added Registration component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HorizontalNav from './features/HorizontalNav';
 import ProductsList from './features/products/ProductsList';
 import ProductDetails from './features/products/SingleProduct';
 import FilteredProducts from './features/products/FilteredProducts';
+import RegisterForm from './features/account/RegisterForm';
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
         <Route path="/products" element={<ProductsList />} />
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/:category" element={<FilteredProducts />} />
-        <Route path="/account" element={<h2>/account</h2>} />
-        <Route path="/cart" element={<h2>/cart</h2>} />
+        <Route path="/users/register" element={<RegisterForm />} />
+        <Route path="/users/account" element={<h2>/account</h2>} />
+        <Route path="/users/cart" element={<h2>/cart</h2>} />
       </Routes>
     </>
   );
---
+++ src/features/account/RegisterForm.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import './index.css'
+
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <div className="registration-container">
+      <h2 className="register-text">Register</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
