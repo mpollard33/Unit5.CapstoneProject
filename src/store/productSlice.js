8fbeaf2b91c22ff27838dc65621c9d273159b59e
mpollard33 <mpollard33@gmail.com>
2023-12-08 00:56:29 -0700

    simplified productSlice for code  consistency

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-
 const productSlice = createSlice({
   name: 'products',
   initialState: {
@@ -14,15 +13,11 @@ const productSlice = createSlice({
     setCategory: (state, { payload }) => {
       state.selectedCategory = payload;
     },
-
-    // addToCart: (state, action) => {
-    //   return state.concat({cart: payload, isEmpty: false})
-    //   state.cart = action.payload; // tentative, .push? .concat
-    // },
   },
 });
 
-export const { setSort, setCategory,  } = productSlice.actions;
+export const { setSort, setCategory } = productSlice.actions;
 export const selectSort = (state) => state.products.sort;
 export const selectCategory = (state) => state.products.selectedCategory;
+
 export default productSlice.reducer;
