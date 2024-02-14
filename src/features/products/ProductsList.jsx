5b01990bab21bd32fa9c1dae75c0c565361fa83b
mpollard33 <mpollard33@gmail.com>
2024-02-13 19:01:24 -0700

    refractored code to better handle conditional queries

--- src/features/products/ProductsList.jsx
+++ src/features/products/ProductsList.jsx
@@ -1,74 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   useGetProductsQuery,
   useGetProductsByCategoryQuery,
 } from './productsApi';
 import VerticalNav from '../VerticalNav';
 import { useSelector } from 'react-redux';
-import './index.css';
-import { selectCategory } from '../../store/productSlice';
 import { useParams } from 'react-router-dom';
+import { selectCategory } from '../../store/productSlice';
 import ProductCard from './ProductCard';
+import './index.css';
 
 const ProductsList = () => {
-  // const { category } = useParams();
-  const category = 'electronics';
-  console.log('category params', category);
-
+  const { category } = useParams();
   const selectedCategory = useSelector(selectCategory);
-  console.log('selectedCategory', category);
+  const categoryQuery = useGetProductsByCategoryQuery(category);
+  const allProductsQuery = useGetProductsQuery();
 
-  const {
-    data: allProducts,
-    error: allProductsError,
-    isLoading: productsLoading,
-  } = useGetProductsQuery();
-
-  const {
-    data: singleCategory,
-    error: singleCategoryError,
-    isLoading: singleCategoryIsLoading,
-  } = useGetProductsByCategoryQuery(category);
-
-  console.log('singleCategory', singleCategory);
-
-  const [products, setProducts] = useState([]);
-
-  const setProductsDisplay = () => {
-    if (category === null || category === undefined || category === '') {
-      return Promise.resolve(allProducts);
-    } else {
-      return Promise.resolve(singleCategory);
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await setProductsDisplay();
-      setProducts(result);
-      console.log('after function', result);
-    };
-
-    fetchData();
-  }, []);
-
-  console.log("products before render", products)
-  if (productsLoading || singleCategoryIsLoading) return <div>Loading...</div>;
-  if (allProductsError || singleCategoryError)
-    return <div>{allProductsError || singleCategoryError}</div>;
+  const query = selectedCategory ? categoryQuery : allProductsQuery;
 
   return (
     <main className="main-container">
       <div className="page-container">
         <VerticalNav />
-        <ul className="product-container">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </ul>
-        <div className="no-products-message">
-          <p>Sorry, there are no products for the selected category.</p>
-        </div>
+        {query.isLoading && <div>Loading...</div>}
+        {query.isError && <div>Error: {query.error.message}</div>}
+        {query.isSuccess && (
+          <ul className="product-container">
+            {query.data.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </ul>
+        )}
+        {query.isSuccess && query.data.length === 0 && (
+          <div className="no-products-message">
+            <p>Sorry, there are no products for the selected category.</p>
+          </div>
+        )}
       </div>
     </main>
   );
